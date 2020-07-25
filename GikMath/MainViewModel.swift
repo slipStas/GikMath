d694f28daf009b6c9f42a0fe735acd7366e36921
slipStas <41761920+slipStas@users.noreply.github.com>
2020-07-25 23:35:43 +0300

    1

--- GikMath/MainViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MainViewModel.swift
-//  GikMath
-//
-//  Created by Stanislav Slipchenko on 25.07.2020.
-//  Copyright © 2020 Stanislav Slipchenko. All rights reserved.
-//
-
-import Foundation
